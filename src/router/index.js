3f3466b8d5cc798f2e05418fb4f93e6cf9ee25fe
xwh <1277529106@qq.com>
2024-10-28 10:36:57 +0800

    10-28 10:36 "完美优化"

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-    {path: '/', name: 'home', component: HomeView},
-    {path: '/about', name: 'about', component: () => import('../views/AboutView.vue')}
+    {path: '/', name: 'home', component: HomeView}
 ]
 
 const router = createRouter({
